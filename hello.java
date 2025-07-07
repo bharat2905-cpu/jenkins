c723589d2ab7ef2a470450752f7719c9144fb1db
Bharat suthar <bharat2905@outlook.com>
2025-07-07 17:26:06 +0530

    some line added

--- hello.java
+++ hello.java
@@ -1,5 +1,11 @@
 public class hello {
 public static void main(String args[]){
     System.out.println("hello buddy!!!");
+
+       int i=1;
+       while (i<=3){
+           System.out.println(i);
+           i++;
+       }
 }
 }
