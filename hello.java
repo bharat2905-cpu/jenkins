9a3dba8144c4a608938c1a12b5038e5799e06478
Bharat suthar <bharat2905@outlook.com>
2025-07-09 13:36:30 +0530

    one change add

--- hello.java
+++ hello.java
@@ -3,7 +3,7 @@ public static void main(String args[]){
     System.out.println("hello buddy!!!");
 
        int i=1;
-       while (i<=5){
+       while (i<=4){
            System.out.println(i);
            i++;
        }
