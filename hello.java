8a91639b011b62c5b39520f9fb9a49c9b9d73b32
Bharat suthar <bharat2905@outlook.com>
2025-07-18 19:14:47 +0530

    new num add

--- hello.java
+++ hello.java
@@ -1,11 +1,11 @@
 public class hello {
-public static void main(String args[]){
-    System.out.println("hello buddy!!!");
+    public static void main(String args[]) {
+        System.out.println("hello buddy!!!");
 
-       int i=1;
-       while (i<=4){
-           System.out.println(i);
-           i++;
-       }
-}
+        int i = 1;
+        while (i <= 5) {
+            System.out.println(i);
+            i++;
+        }
+    }
 }
